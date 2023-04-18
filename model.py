f1c5e69321f1559b7241f594981a985ab5352b01
Gustavo Silvera <grs5382@gmail.com>
2023-04-17 21:25:54 -0400

    add initial model/utils code

---
+++ model.py
@@ -0,0 +1,13 @@
+import torch
+
+from utils import device
+
+
+class GeoGuesser(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        # TODO
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # TODO
+        return x
