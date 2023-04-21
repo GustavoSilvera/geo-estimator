73e9d919dbd7e8188be15b883b0b25e7b174416b
Gustavo Silvera <grs5382@gmail.com>
2023-04-21 09:33:18 -0400

    using primarily gps distance for loss

--- model.py
+++ model.py
@@ -8,10 +8,10 @@ from typing import Tuple, Dict, Optional, List
 batch_size: int = 64  # number of training instances happening at once (in parallel)
 seed: int = 1  # to fix the randomness
 torch.manual_seed(seed)
-epochs: int = 300
+epochs: int = 500
 eval_iter: int = 50
-lr: float = 0.005
-fc_dim: int = 50  # dimensionality of the final fully connected layer
+lr: float = 0.001
+fc_dim: int = 5000  # dimensionality of the final fully connected layer
 dropout: float = 0.2  # percent of indermediate calculations that are disabled
 ckpt_dir: str = "ckpt"
 os.makedirs(ckpt_dir, exist_ok=True)
@@ -33,25 +33,27 @@ class GeoGuesser(torch.nn.Module):
         self.test_idxs = test_idxs
 
         self.im_res = im_res
-        c, w, h = self.im_res
+        c, h, w = self.im_res
 
         # create the network
-        self.out_size = 6  # x, y, z, lat, lon, compass
+        self.out_size = 3  # x, y, z, lat, lon, compass
         conv_dims = [c, 50, 50, 50, 50, 50, 30]
-        conv_kernels = [3, 3, 3, 3, 3, 5]
+        conv_kernels = [7, 5, 5, 5, 5, 5]
         assert len(conv_dims) == 1 + len(conv_kernels)  # includes input channels
+        conv_dims2 = [conv_dims[-1], 30, 30]
+        conv_kernels2 = [3, 3]
 
         class ConvReluBlock(torch.nn.Module):
-            def __init__(self, i):
+            def __init__(self, i: int, dims: List[int], kernels: List[int]):
                 super().__init__()
                 self.network = torch.nn.Sequential(
                     torch.nn.Conv2d(
-                        in_channels=conv_dims[i],
-                        out_channels=conv_dims[i + 1],
-                        kernel_size=conv_kernels[i],
+                        in_channels=dims[i],
+                        out_channels=dims[i + 1],
+                        kernel_size=kernels[i],
                         stride=1,
                         bias=False,
-                        padding=conv_kernels[i] // 2,
+                        padding=kernels[i] // 2,  # maintain shape!
                     ),
                     torch.nn.ReLU(inplace=True),
                     # torch.nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
@@ -62,10 +64,21 @@ class GeoGuesser(torch.nn.Module):
                 return self.network.forward(x)
 
         self.network = torch.nn.Sequential(
-            *[ConvReluBlock(i) for i in range(len(conv_kernels))],
+            *[
+                ConvReluBlock(i, conv_dims, conv_kernels)
+                for i in range(len(conv_kernels))
+            ],
+            torch.nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+            *[
+                ConvReluBlock(i, conv_dims2, conv_kernels2)
+                for i in range(len(conv_kernels2))
+            ],
+            torch.nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
+            torch.nn.ReLU(inplace=True),
             # finall FC7 (layer)
             torch.nn.Flatten(),  # convert to latent vector space for FC layer
-            torch.nn.Linear(conv_dims[-1] * w * h, fc_dim),
+            # torch.nn.Linear(conv_dims[-1] * w * h, fc_dim),
+            torch.nn.Linear(94080, fc_dim),
             torch.nn.Dropout(dropout),
             torch.nn.ReLU(inplace=True),
             torch.nn.Linear(fc_dim, self.out_size),  # final FC layer
@@ -87,10 +100,12 @@ class GeoGuesser(torch.nn.Module):
     def loss_function(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
         assert x.shape == (batch_size, self.out_size)
         assert y.shape == x.shape
-        xyz_pred = x[:, :3]
-        gps_pred = x[:, 3:]
-        xyz_target = y[:, :3]
-        gps_target = y[:, 3:]
+        # xyz_pred = x[:, :3]
+        # gps_pred = x[:, 3:]
+        # xyz_target = y[:, :3]
+        # gps_target = y[:, 3:]
+        gps_pred = x
+        gps_target = y
         # TODO: are we using xyz at all?
         distance_xyz = 0  # self.l2_loss(xyz_pred, xyz_target)
         # TODO: find a better metric for distance of GPS
@@ -105,12 +120,12 @@ class GeoGuesser(torch.nn.Module):
         start_idx = torch.randint(low=1, high=max(data) - 1, size=(batch_size, 1))
         data = [self.dataset[int(i)] for i in start_idx]
         images = torch.stack([image for image, _, _ in data])
-        xyz = torch.stack([xyz for _, xyz, _ in data])
+        # xyz = torch.stack([xyz for _, xyz, _ in data])
         gps = torch.stack([gps for _, _, gps in data])
         assert images.shape == (batch_size, *self.im_res)
-        assert xyz.shape == (batch_size, 3)
+        # assert xyz.shape == (batch_size, 3)
         assert gps.shape == (batch_size, 3)
-        return images, xyz, gps
+        return images, gps
 
     def get_ckpt(self, id: int = -1):
         if id is None or id < 0:
@@ -130,9 +145,9 @@ class GeoGuesser(torch.nn.Module):
         scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer)
         train_loss, val_loss = self.estimate_loss()  # initial losses
         for epoch in range(epochs):
-            img, xyz, gps = self.sample_batch()
-            xyzgps = torch.hstack((xyz, gps))
-            _, loss = self.forward(img, xyzgps)
+            img, gps = self.sample_batch()
+            # xyzgps = torch.hstack((xyz, gps))
+            _, loss = self.forward(img, gps)
             optimizer.zero_grad(set_to_none=True)
             loss.backward()
             optimizer.step()
@@ -146,6 +161,10 @@ class GeoGuesser(torch.nn.Module):
                 train_loss, val_loss = self.estimate_loss(num_iters=eval_iter)
                 torch.save(self.state_dict(), self.get_ckpt(epoch))
                 scheduler.step(loss)
+        print()
+        print(
+            f"Epoch {epochs}/{epochs} \t ({100:.1f}%) \t Train loss: {train_loss:.2f} \t Val loss: {val_loss:.2f}"
+        )
 
     # create a loss estimator for averaging training and val loss
     def estimate_loss(self, num_iters: int = 20) -> Tuple[float, float]:
@@ -155,9 +174,8 @@ class GeoGuesser(torch.nn.Module):
             for split in ["train", "valid"]:
                 cumulative_loss: float = 0
                 for i in range(num_iters):
-                    img, xyz, gps = self.sample_batch(split)
-                    xyzgps = torch.hstack((xyz, gps))
-                    _, loss = self.forward(img, xyzgps)
+                    img, gps = self.sample_batch(split)
+                    _, loss = self.forward(img, gps)
                     cumulative_loss += loss.item()
                     print(
                         f"({split}) Eval: {100 * i / num_iters:.0f}%",
